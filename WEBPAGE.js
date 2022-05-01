1a43b2114ad93b9892ce1a8a029147aecfc01625
Peace Akib <peace.akib@gmail.com>
2022-05-01 18:38:42 +0100

    group task for bnta assessment day march2022

---
+++ WEBPAGE.js
@@ -0,0 +1,8 @@
+// Cems Javascript code, when catwalk vote up button is clicked, button clicked! should appear in console.
+VoteUp = document.getElementById("Vup-button1");
+handleButtonClick = function() {
+    console.log("Button Clicked!");}
+    
+    
+VoteUp.addEventListener("click", handleButtonClick);
+// Cems Javascript code end
